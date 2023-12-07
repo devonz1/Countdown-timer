4f0683853e5f14bb335e216ce59ed124c3da91b8
devon gibson <devongibs2@gmail.com>
2023-12-06 22:46:54 -0800

    updates

--- Countdown Timer/app.js
+++ Countdown Timer/app.js
@@ -1,6 +1,5 @@
 
-document.getElementById("time").innerHTML = days + "d" + hours + "h" + minutes
- + "m" + seconds + "s";
+ 
 
 
 
@@ -22,8 +21,9 @@ document.getElementById("time").innerHTML = days + "d" + hours + "h" + minutes
      // This will get todays date and time
     var now = new Date().getTime();
 
-    // this variable will store the length of time between todays 
-    //date and the countdown date .
+    /* this variable will store the length of time between todays 
+    date and the countdown date  so were subtracting the current time from
+     predetermined time  */
    var distance = countDownDate - now;
  
  /* calculations for Days, hours,minutes  and seconds.
@@ -34,11 +34,18 @@ we will be using Math.floor which will round down whichever number we get
 
  var days = Math.floor(distance/ (1000* 60* 60* 24));
 
- var hours = Math.floor (( distance % (1000 *60 *60*24)) / (1000 * 60) );
+ var hours = Math.floor (( distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
  var minutes = Math.floor(( distance % (1000 * 60 * 60 )) / (1000 * 60));
 
 var seconds = Math.floor((distance % (1000 * 60 )) / 1000);
+
+document.getElementById("time").innerHTML =
+'<div>' + days +'<span>D</span></div>' + 
+'<div>' + hours +'<span>H</span></div>' +
+'<div>' + minutes + '<span> M</span></div>' +
+'<div>' + seconds + '<span> S</span></div>' ;
+ 
 
 if (distance< 0){
     clearInterval(x);
